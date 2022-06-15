338a6332274ba5aed1b4de7cc558f0266dc1d9bf
sherweipng <e0941860@u.nus.edu>
2022-06-15 17:09:51 +0800

    Created data.sql (work in progress)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,42 @@
+INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','ADMIN','capsank');
+INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','ADMIN','capscqr');
+INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','ADMIN','capskh');
+INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','ADMIN','capslbj');
+INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','ADMIN','capstxy');
+INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','ADMIN','capspsw');
+
+INSERT INTO ADMINISTRATOR(admin_id,firstName,lastName, account_id) VALUES (default,'Nyi Khaing','Aung',1);
+INSERT INTO ADMINISTRATOR(admin_id,firstName,lastName, account_id) VALUES (default,'Qier','Chu',2);
+INSERT INTO ADMINISTRATOR(admin_id,firstName,lastName, account_id) VALUES (default,'Hein','Kaung',3);
+INSERT INTO ADMINISTRATOR(admin_id,firstName,lastName, account_id) VALUES (default,'Bao Jin','Lee', 4);
+INSERT INTO ADMINISTRATOR(admin_id,firstName,lastName, account_id) VALUES (default,'Xin Yi','Tay', 5);
+INSERT INTO ADMINISTRATOR(admin_id,firstName,lastName, account_id) VALUES (default,'Sher Wei','Png', 6);
+
+INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','LECTURER','chandra');
+INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','LECTURER','yuenkwan');
+INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','LECTURER','darrylng');
+INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','LECTURER','esthertan');
+INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','LECTURER','boonkui');
+INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','LECTURER','kennethphang');
+INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','LECTURER','chukmun');
+INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','LECTURER','munkew');
+INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','LECTURER','fanliu');
+INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','LECTURER','tritin');
+INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','LECTURER','suriyapriya');
+INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','LECTURER','cherwah');
+INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','LECTURER','venkatramanathan');
+
+INSERT INTO LECTURER(lecturer_Id,firstName,lastName,account_id) VALUES (default,'Chandrasekaran','Paramasivam',7);
+INSERT INTO LECTURER(lecturer_Id,firstName,lastName,account_id) VALUES (default,'Yuen Kwan','Chia',8);
+INSERT INTO LECTURER(lecturer_Id,firstName,lastName,account_id) VALUES (default,'Darryl','Ng',9);
+INSERT INTO LECTURER(lecturer_Id,firstName,lastName,account_id) VALUES (default,'Esther','Tan',10);
+INSERT INTO LECTURER(lecturer_Id,firstName,lastName,account_id) VALUES (default,'Boon Kui','Heng',11);
+INSERT INTO LECTURER(lecturer_Id,firstName,lastName,account_id) VALUES (default,'Kenneth','Phang',12);
+INSERT INTO LECTURER(lecturer_Id,firstName,lastName,account_id) VALUES (default,'Chuk Mun','Lee',13);
+INSERT INTO LECTURER(lecturer_Id,firstName,lastName,account_id) VALUES (default,'Mun Kew','Leong',14);
+INSERT INTO LECTURER(lecturer_Id,firstName,lastName,account_id) VALUES (default,'Fan','Liu',15);
+INSERT INTO LECTURER(lecturer_Id,firstName,lastName,account_id) VALUES (default,'Tri Tin','Nguyen',16);
+INSERT INTO LECTURER(lecturer_Id,firstName,lastName,account_id) VALUES (default,'Suriya Priya','Asaithambi',17);
+INSERT INTO LECTURER(lecturer_Id,firstName,lastName,account_id) VALUES (default,'Cher Wah','Tan',18);
+INSERT INTO LECTURER(lecturer_Id,firstName,lastName,account_id) VALUES (default,'Venkat','Ramanathan',19);
+
