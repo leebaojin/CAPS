7ebe76f4653acf657c52c922036dfa1c5c14c844
sherweipng <e0941860@u.nus.edu>
2022-06-15 17:29:55 +0800

    Created inserts for courses

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,12 +5,14 @@ INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password
 INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','ADMIN','capstxy');
 INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','ADMIN','capspsw');
 
+
 INSERT INTO ADMINISTRATOR(admin_id,firstName,lastName, account_id) VALUES (default,'Nyi Khaing','Aung',1);
 INSERT INTO ADMINISTRATOR(admin_id,firstName,lastName, account_id) VALUES (default,'Qier','Chu',2);
 INSERT INTO ADMINISTRATOR(admin_id,firstName,lastName, account_id) VALUES (default,'Hein','Kaung',3);
 INSERT INTO ADMINISTRATOR(admin_id,firstName,lastName, account_id) VALUES (default,'Bao Jin','Lee', 4);
 INSERT INTO ADMINISTRATOR(admin_id,firstName,lastName, account_id) VALUES (default,'Xin Yi','Tay', 5);
 INSERT INTO ADMINISTRATOR(admin_id,firstName,lastName, account_id) VALUES (default,'Sher Wei','Png', 6);
+
 
 INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','LECTURER','chandra');
 INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','LECTURER','yuenkwan');
@@ -26,6 +28,7 @@ INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password
 INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','LECTURER','cherwah');
 INSERT INTO ACCOUNT(account_id,password,role,username) VALUES (default,'password-4','LECTURER','venkatramanathan');
 
+
 INSERT INTO LECTURER(lecturer_Id,firstName,lastName,account_id) VALUES (default,'Chandrasekaran','Paramasivam',7);
 INSERT INTO LECTURER(lecturer_Id,firstName,lastName,account_id) VALUES (default,'Yuen Kwan','Chia',8);
 INSERT INTO LECTURER(lecturer_Id,firstName,lastName,account_id) VALUES (default,'Darryl','Ng',9);
@@ -40,3 +43,17 @@ INSERT INTO LECTURER(lecturer_Id,firstName,lastName,account_id) VALUES (default,
 INSERT INTO LECTURER(lecturer_Id,firstName,lastName,account_id) VALUES (default,'Cher Wah','Tan',18);
 INSERT INTO LECTURER(lecturer_Id,firstName,lastName,account_id) VALUES (default,'Venkat','Ramanathan',19);
 
+
+INSERT INTO COURSE(course_id,course_capacity,course_credits,course_description,course_status,course_title) VALUES (default,120,8,'Use HTML5, CSS3, Flexbox, Grid & SASS to build website content and add stunning styling and decoration. Use Javascript, jQuery & jQuery User Interface to create Interactive Websites and Games.','open','The Complete Web Development Course -  Build 15 Projects');
+INSERT INTO COURSE(course_id,course_capacity,course_credits,course_description,course_status,course_title) VALUES (default,100,8,'Students will be able to learn all the essential functions of PHP & MySQL and their proper usage. Students will be able to make any kind of modern multi vendors e-commerce store in php&mysqli with the assistance of Bootstrap 3.','open','Modern E-Commerce Store In php & mysqli With Bootstrap');
+INSERT INTO COURSE(course_id,course_capacity,course_credits,course_description,course_status,course_title) VALUES (default,100,8,'To learn every skill needed as a professional web developer/designer. To create real life projects for your portfolio. To become a professional web developer.','open','Become a Professional Web Developer - Version 3.0');
+INSERT INTO COURSE(course_id,course_capacity,course_credits,course_description,course_status,course_title) VALUES (default,80,6,'Learn how to build both "fast-twitch" and "minimally invasive" income streams manage clients more efficiently and make more money per project merge.','open','Code & Grow Rich:  Earn More As An Entrepreneur Or Developer');
+INSERT INTO COURSE(course_id,course_capacity,course_credits,course_description,course_status,course_title) VALUES (default,80,6,'Learn the fundamentals of coding! Create beautiful interactive websites from scratch using the framework of your choice! Become an expert using HTML, CSS, Wordpress, Bootstrap, PHP, AJAX, jQuery, and So Much More!','open','The Complete Web Developer Masterclass: Beginner To Advanced');
+INSERT INTO COURSE(course_id,course_capacity,course_credits,course_description,course_status,course_title) VALUES (default,60,6,'Learn to create websites using a wysiwyg HTML editor. Learn Basic HTML & CSS. Learn how to develop using PHP and MySQL. Advanced web development languages including JavaScript, jQuery and XML. Learn essentials for AngularJS.','open','Become a Kick-Ass Web Developer: From Newbie to Pro');
+INSERT INTO COURSE(course_id,course_capacity,course_credits,course_description,course_status,course_title) VALUES (default,60,6,'Learn the Python Programming Language. Learn the Django & Twitter Bootstrap frameworks. Learn how to use APIs. Learn how to accept payments via Stripe. Learn Basic HTML & CSS.','open','Coding for Entrepreneurs: Learn Python, Django, and More.');
+INSERT INTO COURSE(course_id,course_capacity,course_credits,course_description,course_status,course_title) VALUES (default,60,6,'To learn HTML, HTML 5, CSS, CSS3, C++, C#, jQuery, Game Salad, Stencyl. To learn great development techniques. Learn how to make apps in Xcode. Learn how to make games in Construct 2. Learn adobe illustrator.','open','Back to School Web Development and Programming Bundle');
+INSERT INTO COURSE(course_id,course_capacity,course_credits,course_description,course_status,course_title) VALUES (default,60,6,'Build sites based on latest and new tips trick with CSS. Complete understanding of the particular topic i will cover on this course. Able to use CSS libraries and plugins fluently. Able to use request a new videos on particular topic in CSS.','open','CSSCasts; CSS libraries Plugins Tips & Tricks for Developers');
+INSERT INTO COURSE(course_id,course_capacity,course_credits,course_description,course_status,course_title) VALUES (default,60,6,'The ins and outs of HTML5, CSS3, and Modern JavaScript for 2021. Make REAL web applications using cutting-edge technologies. Create responsive, accessible, and beautiful layouts. Recognize and prevent common security exploits like SQL-Injection & XSS.','open','The Web Developer Bootcamp');
+INSERT INTO COURSE(course_id,course_capacity,course_credits,course_description,course_status,course_title) VALUES (default,60,6,'After Completing this Course , you will have pretty good knowledge about HTML , CSS , Javascript , Jquery , Ajax, Bootstrap, PHP, MYSQL , Wordpress. You will Learn How to Create Multiuser Server Based Photogallery Site.','open','The Most Comprehensive Web Development Course');
+INSERT INTO COURSE(course_id,course_capacity,course_credits,course_description,course_status,course_title) VALUES (default,40,4,'Implement advanced JavaScript components, such as persistent drag and drop functionality and live page update via ActionCable into a Rails application. Deploying a Rails application along with the ActionCable web socket feature to Heroku.','open','Dissecting Ruby on Rails 5 - Become a Professional Developer');
+INSERT INTO COURSE(course_id,course_capacity,course_credits,course_description,course_status,course_title) VALUES (default,40,4,'Learn core concepts of both the front end and backend programming course. Get familiar with the latest web developer technologies and ecosystems. Learn all about sql and nosql databses. Learn complete web development process.','open','The Full Stack Web Development');
