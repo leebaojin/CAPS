e86e9d8148afb44e9afd9858c862b4bb63cc0f09
Kaung Hein <kaunghein963@gmail.com>
2022-06-18 19:27:00 +0800

    Done CRUD Process for Student
    Still trying to figure out how to store Date in dd-mm-yyyy format

--- src/main/java/sg/edu/iss/caps/controller/StudentManagementController.java
+++ src/main/java/sg/edu/iss/caps/controller/StudentManagementController.java
@@ -5,8 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import sg.edu.iss.caps.model.Role;
 import sg.edu.iss.caps.model.Student;
+import sg.edu.iss.caps.model.UserStatus;
 import sg.edu.iss.caps.repo.StudentRepository;
+import sg.edu.iss.caps.util.HashUtil;
 
 import java.util.List;
 
@@ -32,18 +36,39 @@ public class StudentManagementController {
 		{
 			return "student.studentId";
 		}
-    	studentRepo.save(s);
+    	
+    	//Creating a new Student Code Block
+    	if(s.getStudentId() != null)
+    	{
+    		//For Editing the existing Student
+    		Student newStudent = studentRepo.findById(s.getStudentId()).get();
+    		newStudent.setFirstName(s.getFirstName());
+    		newStudent.setLastName(s.getLastName());
+    		newStudent.setUsername(s.getUsername());
+    		newStudent.setEmail(s.getEmail());
+//    		newStudent.setEnrolledDate(s.getEnrolledDate());
+    		studentRepo.save(newStudent);
+    	}
+    	else 
+    	{
+    		//For Adding a new student
+			s.setRole(Role.STUDENT);
+			s.setUserStatus(UserStatus.ACTIVE);
+			String defaultPwd = "123456";
+			s.setPasswordHash(HashUtil.getHash(s.getUsername(),defaultPwd));
+			studentRepo.save(s);
+		}
         return "forward:/student-management/liststudents";
     }
 
-    @GetMapping("/liststudents")
+    @RequestMapping("/liststudents")
     public String listStudents(Model model) {
-        List<Student> studentList = studentRepo.findAll();
+        List<Student> studentList = studentRepo.findAllActiveStudents();
         model.addAttribute("studentList", studentList);
         return "list-students";
     }
 
-    @GetMapping("/liststudents/{name}")
+    @GetMapping("/{name}")
     public String listStudentsByName(Model model, @PathVariable("name") String name) {
         List<Student> studentList = studentRepo.findStudentByFirstName(name);
         model.addAttribute("studentList", studentList);
@@ -59,7 +84,11 @@ public class StudentManagementController {
     @GetMapping("/delete/{id}")
     public String deleteStudent(Model model, @PathVariable("id") Integer id) {
         Student s = studentRepo.findById(id).get();
-        studentRepo.delete(s);
+        if(s.getStudentId() != null)
+        {
+        	s.setUserStatus(UserStatus.INACTIVE);
+        	studentRepo.save(s);
+        }
         return "forward:/student-management/liststudents";
     }
 }
