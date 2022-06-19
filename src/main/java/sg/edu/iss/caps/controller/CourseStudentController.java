5d65d9c1364354d80471f3742aa7ff213c730018
sherweipng <e0941860@u.nus.edu>
2022-06-19 19:22:11 +0800

    CourseStudentController (Work in progress)

--- src/main/java/sg/edu/iss/caps/controller/CourseStudentController.java
+++ src/main/java/sg/edu/iss/caps/controller/CourseStudentController.java
@@ -1,13 +1,19 @@
 package sg.edu.iss.caps.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import sg.edu.iss.caps.model.CourseStudent;
 import sg.edu.iss.caps.model.Grade;
+import sg.edu.iss.caps.model.Lecturer;
+import sg.edu.iss.caps.model.Role;
+import sg.edu.iss.caps.model.UserStatus;
 import sg.edu.iss.caps.repo.CourseRepository;
 import sg.edu.iss.caps.repo.CourseStudentRepository;
+import sg.edu.iss.caps.util.HashUtil;
 
 @Controller
 @RequestMapping("/course-student")
@@ -17,25 +23,59 @@ public class CourseStudentController {
     CourseStudentRepository courseStudentRepo;
     CourseRepository courseRepo;
 
-    @GetMapping("/{id}/list")
-    public String listCourseStudent(Model model, @PathVariable("id") Integer courseStudentId) {
-        CourseStudent courseStudent = courseStudentRepo.findById(courseStudentId).get();
-        model.addAttribute("courseStudent", courseStudent);
-        return "list-coursestudent";
+    @RequestMapping("/list")
+    public String listCourseStudents(Model model) {
+        List<CourseStudent> courseStudentList = courseStudentRepo.findAll();
+        model.addAttribute("courseStudentList", courseStudentList);
+        return "list-coursestudents";
     }
+    
+    @GetMapping("/add-grade/{courseStudentId}")
+    public String addGrade(Model model, @PathVariable("courseStudentId") Integer courseStudentId) {
+    	model.addAttribute("courseStudent", courseStudentRepo.findById(courseStudentId).get());
+        return "courseStudent-form";
+    }
+    
+    @PostMapping("/save")
+    public String saveCourseStudentForm(@ModelAttribute("courseStudent")  CourseStudent cs) {
+    		CourseStudent cs2 = courseStudentRepo.findById(cs.getCourseStudentId()).get();
+    		cs2.setCourseStudentId(cs.getCourseStudentId());
+    		cs2.setCourse(cs.getCourse());
+    		cs2.setStudent(cs.getStudent());
+    		courseStudentRepo.save(cs2);
+        return "forward:/course-student/list";
+    }
+    
+//public static void  calculateGrade(int score) {
+//	
+//	Grade g;
+//	
+//	if(score >= 90 || score <= 100) {
+//		g = Grade.A;
+//	} else if (score >= 80 || score <= 89){
+//		g = Grade.B;
+//	} else if (score >= 70 || score <= 79) {
+//		g = Grade.C;
+//	} else if (score >= 60 || score <= 69) {
+//		g = Grade.D;
+//	} else if (score >= 0 || score <= 59) {
+//		g = Grade.F;
+//	}
 
-    @GetMapping("/{id}/view-grade")
-    public String viewGrade(Model model, @PathVariable("id") Integer courseStudentId) {
-        CourseStudent courseStudent = courseStudentRepo.findById(courseStudentId).get();
-        model.addAttribute("courseStudent", courseStudent);
-        return "coursestudent";
-    }
+    
+//    @GetMapping("/{id}/list")
+//    public String listCourseStudent(Model model, @PathVariable("id") Integer courseStudentId) {
+//        CourseStudent courseStudent = courseStudentRepo.findById(courseStudentId).get();
+//        model.addAttribute("courseStudent", courseStudent);
+//        return "list-coursestudents";
+//    }
+//
+//    @GetMapping("/{id}/view-grade")
+//    public String viewGrade(Model model, @PathVariable("id") Integer courseStudentId) {
+//        CourseStudent courseStudent = courseStudentRepo.findById(courseStudentId).get();
+//        model.addAttribute("courseStudent", courseStudent);
+//        return "coursestudent";
+//    }
 
-    @PostMapping("/{id}/add-grade")
-    public String addGrade(Model model, @PathVariable("id") Integer courseStudentId, @ModelAttribute("grade") Grade g) {
-        CourseStudent courseStudent = courseStudentRepo.findById(courseStudentId).get();
-        courseStudent.setGrade(g);
-        courseStudentRepo.save(courseStudent);
-        return "forward:/{id}/view-grade";
-    }
+
 }
