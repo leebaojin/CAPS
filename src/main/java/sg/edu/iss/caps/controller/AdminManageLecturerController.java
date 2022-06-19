f1c80bd8427727fa6944b4fc300b918cc880ff1a
sherweipng <e0941860@u.nus.edu>
2022-06-20 00:20:38 +0800

    Amended methods for correct url mapping

--- src/main/java/sg/edu/iss/caps/controller/AdminManageLecturerController.java
+++ src/main/java/sg/edu/iss/caps/controller/AdminManageLecturerController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
 import sg.edu.iss.caps.model.Lecturer;
 import sg.edu.iss.caps.model.Role;
 import sg.edu.iss.caps.model.UserStatus;
@@ -17,7 +20,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 @Controller
-@RequestMapping("/lecturer-management")
+@RequestMapping("/manage/lecturer") 
 public class AdminManageLecturerController {
 
     @Autowired
@@ -27,26 +30,48 @@ public class AdminManageLecturerController {
     public String loadLecturerForm(Model model) {
         Lecturer l = new Lecturer();
         model.addAttribute("lecturer",l);
+        model.addAttribute("action","create");
         return "lecturer-form";
     }
     
-    @PostMapping("/save")
-    public String saveLecturerForm(@ModelAttribute("lecturer")  Lecturer l) {
-    	if (l.getLecturerId() != null) {
-    		Lecturer l2 = lecturerRepo.findById(l.getLecturerId() ).get();
-    		l2.setFirstName(l.getFirstName());
-    		l2.setLastName(l.getLastName());
-    		l2.setUsername( l.getUsername());
-    		l2.setEmail(l.getEmail());
-    		lecturerRepo.save(l2);
-    	} else {
-    		l.setRole(Role.LECTURER);
-    		l.setUserStatus(UserStatus.ACTIVE);
-    		String defaultPwd = "123456";
-    		l.setPasswordHash(HashUtil.getHash(l.getUsername(),defaultPwd));
-    		lecturerRepo.save(l);
-    	}
-        return "forward:/lecturer-management/list";
+    @PostMapping("/create")
+    public String loadLecturerForm(@ModelAttribute("lecturer") @Valid Lecturer l, BindingResult bindingResult, Model model) {
+    	model.addAttribute("action","create");
+    	if(bindingResult.hasErrors()) {
+			return "lecturer-form";
+		}
+		l.setRole(Role.LECTURER);
+		l.setUserStatus(UserStatus.ACTIVE);
+		String defaultPwd = "123456";
+		l.setPasswordHash(HashUtil.getHash(l.getUsername(),defaultPwd));
+		lecturerRepo.save(l);
+        
+        return "redirect:/manage/lecturer/list";
+    }
+    
+    @GetMapping("/edit/{lecturerId}")
+    public String loadEditLecturerForm(Model model, @PathVariable("lecturerId") Integer lecturerId) {
+    	model.addAttribute("lecturer", lecturerRepo.findById(lecturerId).get());
+    	 model.addAttribute("action","edit");
+        return "lecturer-form";
+    }
+    
+    @PostMapping("/edit/{lecturerId}")
+    public String loadEditLecturerForm(@ModelAttribute("lecturer") @Valid Lecturer l, BindingResult bindingResult, Model model) {
+    	 model.addAttribute("action","edit");
+
+		if(bindingResult.hasErrors()) {
+			return "lecturer-form";
+		}
+		Lecturer l2 = lecturerRepo.findById(l.getLecturerId() ).get();
+		l2.setFirstName(l.getFirstName());
+		l2.setLastName(l.getLastName());
+		l2.setUsername( l.getUsername());
+		l2.setEmail(l.getEmail());
+		lecturerRepo.save(l2);
+		
+        model.addAttribute("action","edit");
+        return "redirect:/manage/lecturer/list";
     }
 
     @RequestMapping("/list")
@@ -56,29 +81,6 @@ public class AdminManageLecturerController {
         return "list-lecturers";
     }
 
-	@GetMapping("/{lecturerId}")
-	public String displayDetails(@PathVariable("lecturerId") String lecturerId, Model model) {
-		List<Integer> displayLecturer = new ArrayList<Integer>();
-		displayLecturer.add(Integer.parseInt(lecturerId));
-		List<Lecturer> lecturerList = lecturerRepo.findAllById(displayLecturer);
-		System.out.println(lecturerList.get(0));
-		model.addAttribute("lecturerList", lecturerList);
-		return "list-lecturers";
-	}
-
-    // clarify purpose of editLecturerPage
-//    @GetMapping("/edit-page")
-//    public String editLecturerPage(Model model) {
-//        return "edit-lecturer";
-//    }
-
-    // clarify difference between this and editLecturerPage
-    @GetMapping("/edit/{lecturerId}")
-    public String editLecturer(Model model, @PathVariable("lecturerId") Integer lecturerId) {
-        model.addAttribute("lecturer", lecturerRepo.findById(lecturerId).get());
-        return "lecturer-form";
-    }
-
     @GetMapping("/delete/{lecturerId}")
     public String deleteLecturer(Model model, @PathVariable("lecturerId") Integer lecturerId) {
     	Lecturer l2 = lecturerRepo.findById(lecturerId ).get();
@@ -86,6 +88,60 @@ public class AdminManageLecturerController {
     		l2.setUserStatus(UserStatus.INACTIVE);
     		lecturerRepo.save(l2);
     	}
-        return "forward:/lecturer-management/list";
+        return "redirect:/manage/lecturer/list";
+
     }
+    
+//	@GetMapping("/{lecturerId}")
+//	public String displayDetails(@PathVariable("lecturerId") String lecturerId, Model model) {
+//		List<Integer> displayLecturer = new ArrayList<Integer>();
+//		displayLecturer.add(Integer.parseInt(lecturerId));
+//		List<Lecturer> lecturerList = lecturerRepo.findAllById(displayLecturer);
+//		System.out.println(lecturerList.get(0));
+//		model.addAttribute("lecturerList", lecturerList);
+//		return "list-lecturers";
+//	}
+//	
+//	@PostMapping("/save")
+//    public String saveLecturerForm(@ModelAttribute("lecturer") @Valid Lecturer l, BindingResult bindingResult, Model model) {
+//    	
+//    	if (l.getLecturerId() != null) {
+//    		if(bindingResult.hasErrors()) {
+//    			//return "forward:/manage/lecturer/create";
+//    			return "lecturer-form";
+//    		}
+//    		Lecturer l2 = lecturerRepo.findById(l.getLecturerId() ).get();
+//    		l2.setFirstName(l.getFirstName());
+//    		l2.setLastName(l.getLastName());
+//    		l2.setUsername( l.getUsername());
+//    		l2.setEmail(l.getEmail());
+//    		lecturerRepo.save(l2);
+//    	} else {
+//    		if(bindingResult.hasErrors()) {
+//
+//    			//return "forward:/manage/lecturer/create";
+//    			return "lecturer-form";
+//    		}
+//    		l.setRole(Role.LECTURER);
+//    		l.setUserStatus(UserStatus.ACTIVE);
+//    		String defaultPwd = "123456";
+//    		l.setPasswordHash(HashUtil.getHash(l.getUsername(),defaultPwd));
+//    		lecturerRepo.save(l);
+//    	}
+////        return "forward:/lecturer-management/list";
+//        return "forward:/manage/lecturer/list";
+//
+//    }
+    // clarify purpose of editLecturerPage
+//    @GetMapping("/edit-page")
+//    public String editLecturerPage(Model model) {
+//        return "edit-lecturer";
+//    }
+
+    // clarify difference between this and editLecturerPage
+//    @GetMapping("/edit/{lecturerId}")
+//    public String editLecturer(Model model, @PathVariable("lecturerId") Integer lecturerId) {
+//        model.addAttribute("lecturer", lecturerRepo.findById(lecturerId).get());
+//        return "lecturer-form";
+//    }
 }
