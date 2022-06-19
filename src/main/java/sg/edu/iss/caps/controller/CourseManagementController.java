3e71aa4c867f80af652e2876912ced496fd6574e
Xin Yi Tay <xinzwerkz@gmail.com>
2022-06-19 18:46:14 +0800

    added add lecturer functionality (only 1 lecturer for now)

--- src/main/java/sg/edu/iss/caps/controller/CourseManagementController.java
+++ src/main/java/sg/edu/iss/caps/controller/CourseManagementController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import sg.edu.iss.caps.model.*;
 import sg.edu.iss.caps.repo.CourseRepository;
+import sg.edu.iss.caps.repo.LecturerRepository;
 
 import java.util.List;
 
@@ -16,22 +17,29 @@ public class CourseManagementController {
     @Autowired
     CourseRepository courseRepo;
 
+    @Autowired
+    LecturerRepository lecturerRepo;
+
     @GetMapping("/create")
     public String createCoursePage(Model model) {
         Course c = new Course();
+        List<Lecturer> lecturerList = lecturerRepo.findAll();
         model.addAttribute("course", c);
+        model.addAttribute("lecturerList", lecturerList);
         return "course-form";
     }
 
     @RequestMapping("/list")
     public String listCourses(Model model) {
         List<Course> courseList = courseRepo.findAll();
+        List<Lecturer> lecturerList = lecturerRepo.findAll();
         model.addAttribute("courseList", courseList);
+        model.addAttribute("lecturerList", lecturerList);
         return "list-courses";
     }
 
     @PostMapping("/save")
-    // validation tbc
+    // validation tbc: courseCode
     public String saveCourse(@ModelAttribute("course") Course c) {
         // if course present, get course object, otherwise create new course
         Course c2 = courseRepo.findById(c.getCourseCode()).isPresent() ?
@@ -43,6 +51,7 @@ public class CourseManagementController {
             c2.setCourseCredits(c.getCourseCredits());
             c2.setCourseCapacity(c.getCourseCapacity());
             c2.setCourseStatus(c.getCourseStatus());
+            c2.setCourseLecturers(c.getCourseLecturers());
             courseRepo.save(c2);
         return "forward:/course/list";
     }
@@ -57,7 +66,9 @@ public class CourseManagementController {
     @GetMapping("/edit/{courseId}")
     public String editCourse(Model model, @PathVariable("courseId") String courseId) {
         Course c = courseRepo.findById(courseId).get();
+        List<Lecturer> lecturerList = lecturerRepo.findAll();
         model.addAttribute("course", c);
+        model.addAttribute("lecturerList", lecturerList);
         return "course-form";
     }
 
@@ -69,12 +80,13 @@ public class CourseManagementController {
     }
 
     // can add validation for the form
-    @GetMapping("/{id}/lecturers")
-    public String listCourseLecturers(Model model, @PathVariable("id") String id) {
-        List<Lecturer> lecturerList = courseRepo.findById(id).get().getCourseLecturers();
-        model.addAttribute("lecturers", lecturerList);
-        return "/lecturers";
-    }
+//    @GetMapping("/{id}/lecturers")
+//    public String listCourseLecturers(Model model, @PathVariable("id") String id) {
+//        Course c = courseRepo.findById(id).get();
+//        List<Lecturer> lecturerList = courseRepo.findById(id).get().getCourseLecturers();
+//        model.addAttribute("lecturers", lecturerList);
+//        return "course-info";
+//    }
 
     @PostMapping("/{id}/add-lecturer")
     public String addCourseLecturer(Model model, @PathVariable("id") String id, @ModelAttribute("lecturer") Lecturer l) {
