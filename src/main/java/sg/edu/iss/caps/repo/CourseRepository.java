e99c927fa60697dce0feac7c863992244d2d0f1f
sherweipng <e0941860@u.nus.edu>
2022-06-20 22:36:15 +0800

    Added a query to CourseRepo

--- src/main/java/sg/edu/iss/caps/repo/CourseRepository.java
+++ src/main/java/sg/edu/iss/caps/repo/CourseRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import sg.edu.iss.caps.model.Course;
 import sg.edu.iss.caps.model.Student;
@@ -19,6 +20,9 @@ public interface CourseRepository extends JpaRepository<Course, String> {
 
 	@Query("SELECT c FROM Course c WHERE c IN(SELECT c FROM Course c  JOIN c.courseStudents cs WHERE cs.student = :s)")
 	public List<Course> findCourseTaken(Student s);
+	
+	@Query("Select c from Course c where course_code = :courseCode")
+	Course findCourseCode(@Param("courseCode") String courseCode);
 	
 //	@Query("SELECT c FROM Course c WHERE c IN(SELECT c FROM Course c  JOIN c.courseStudents cs WHERE cs.student = :s AND cs.courseStudentStatus IN ('COMPLETED','FAILED'))")
 //	public List<Course> findGrades(Student s);
