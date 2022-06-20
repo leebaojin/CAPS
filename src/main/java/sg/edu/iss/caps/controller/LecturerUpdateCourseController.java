d532a22383a150b1c5cd8b68e103651a19ea9ea9
sherweipng <e0941860@u.nus.edu>
2022-06-20 22:35:37 +0800

    LecturerUpdateCourseController + views (Work in progress)

--- src/main/java/sg/edu/iss/caps/controller/LecturerUpdateCourseController.java
+++ src/main/java/sg/edu/iss/caps/controller/LecturerUpdateCourseController.java
@@ -3,61 +3,102 @@ package sg.edu.iss.caps.controller;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import sg.edu.iss.caps.model.Course;
 import sg.edu.iss.caps.model.CourseStudent;
+import sg.edu.iss.caps.model.Grade;
+import sg.edu.iss.caps.model.Lecturer;
+import sg.edu.iss.caps.model.Role;
 import sg.edu.iss.caps.model.User;
+import sg.edu.iss.caps.model.UserStatus;
 import sg.edu.iss.caps.repo.CourseRepository;
 import sg.edu.iss.caps.repo.CourseStudentRepository;
+import sg.edu.iss.caps.repo.LecturerRepository;
 import sg.edu.iss.caps.service.UserSessionService;
+import sg.edu.iss.caps.util.HashUtil;
 import sg.edu.iss.caps.util.MenuNavBarUtil;
 
 @Controller
-@RequestMapping("/course-student")
+@RequestMapping("/lecturer")
 public class LecturerUpdateCourseController {
 
     @Autowired
     CourseStudentRepository courseStudentRepo;
+    @Autowired
     CourseRepository courseRepo;
+    @Autowired
+    LecturerRepository lecturerRepo;
+    
 
-    @RequestMapping("/list")
-    public String listCourseStudents(HttpSession session, Model model) {
-    	User user = UserSessionService.findUser(session);
+    @RequestMapping("/list-courses")
+    public String listTeachCourses(HttpSession session, Model model, Lecturer l) {
+    	Lecturer user = (Lecturer) UserSessionService.findUser(session);
     	MenuNavBarUtil.generateNavBar(user, model);
-    	
-        List<CourseStudent> courseStudentList = courseStudentRepo.findAll();
-        model.addAttribute("courseStudentList", courseStudentList);
-        return "list-coursestudents";
+
+    	Lecturer l2 = lecturerRepo.findById(user.getLecturerId()).get();
+    	List<Course>teachCoursesList = l2.getTeachCourses();
+        model.addAttribute("teachCoursesList", teachCoursesList);
+        return "list-lecturer-courses";
     }
     
-    @GetMapping("/add-grade/{courseStudentId}")
-    public String addGrade(HttpSession session, Model model, @PathVariable("courseStudentId") Integer courseStudentId) {
+    @RequestMapping("/list-course-students/{courseCode}")
+    public String listCourseStudents(HttpSession session, Model model, @PathVariable("courseCode") String courseCode) {
     	User user = UserSessionService.findUser(session);
     	MenuNavBarUtil.generateNavBar(user, model);
     	
+        List<CourseStudent> courseStudentList = courseStudentRepo.findCSByCourse(courseCode);
+        model.addAttribute("courseStudentList", courseStudentList);
+        Course c = courseRepo.findCourseCode(courseCode);
+        model.addAttribute("c", c);
+        return "list-courses-students";
+    }
+    
+    @GetMapping("/add-score/{courseStudentId}")
+    public String loadAddScoreForm(HttpSession session, Model model, @PathVariable("courseStudentId") Integer courseStudentId) {
+    	User user = UserSessionService.findUser(session);
+    	MenuNavBarUtil.generateNavBar(user, model);
+
     	model.addAttribute("courseStudent", courseStudentRepo.findById(courseStudentId).get());
         return "courseStudent-form";
     }
     
-    @PostMapping("/save")
-    public String saveCourseStudentForm(@ModelAttribute("courseStudent")  CourseStudent cs, HttpSession session) {
-    	User user = UserSessionService.findUser(session);
-    	
-    	CourseStudent cs2 = courseStudentRepo.findById(cs.getCourseStudentId()).get();
-    		cs2.setCourseStudentId(cs.getCourseStudentId());
-    		cs2.setCourse(cs.getCourse());
-    		cs2.setStudent(cs.getStudent());
-    		courseStudentRepo.save(cs2);
-        return "forward:/course-student/list";
-    }
+//    @PostMapping("/add-score/{courseStudentId}")
+//    public String saveAddScoreForm(HttpSession session, Model model, @ModelAttribute("courseStudent") CourseStudent cs) {
+//    	User user = UserSessionService.findUser(session);
+//    	MenuNavBarUtil.generateNavBar(user, model);
+//    	
+//    	Student s = 
+//		CourseStudent cs2 = courseStudentRepo.findById(cs.).get();
+//		l2.setFirstName(l.getFirstName());
+//		l2.setLastName(l.getLastName());
+//		l2.setUsername( l.getUsername());
+//		l2.setEmail(l.getEmail());
+//		lecturerRepo.save(l2);
+//		
+//        model.addAttribute("action","edit");
+//        return "redirect:/manage/lecturer/list";
+//    }
+    
+    
+    
+//    @PostMapping("/save")
+//    public String saveCourseStudentForm(@ModelAttribute("courseStudent")  CourseStudent cs, HttpSession session) {
+//    	User user = UserSessionService.findUser(session);
+//    	
+//    	CourseStudent cs2 = courseStudentRepo.findById(cs.getCourseStudentId()).get();
+//    		cs2.setCourseStudentId(cs.getCourseStudentId());
+//    		cs2.setCourse(cs.getCourse());
+//    		cs2.setStudent(cs.getStudent());
+//    		courseStudentRepo.save(cs2);
+//        return "forward:/course-student/list";
+//    }
     
 //public static void  calculateGrade(int score) {
 //	
--- src/main/java/sg/edu/iss/caps/repo/CourseStudentRepository.java
+++ src/main/java/sg/edu/iss/caps/repo/CourseStudentRepository.java
@@ -1,9 +1,11 @@
 package sg.edu.iss.caps.repo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import sg.edu.iss.caps.model.Course;
 import sg.edu.iss.caps.model.CourseStudent;
@@ -14,4 +16,6 @@ public interface CourseStudentRepository extends JpaRepository<CourseStudent, In
 	@Query("SELECT cs FROM CourseStudent cs WHERE cs.student = :s AND cs.course = :c")
 	public List<CourseStudent> findCSByStudentAndCourse(Student s, Course c);
 	
+	@Query("SELECT cs FROM CourseStudent cs WHERE course_course_code = :courseCode")
+	ArrayList<CourseStudent>findCSByCourse(@Param("courseCode") String courseCode);
 }
